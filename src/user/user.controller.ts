8b5fab52b09c67adf3a43e52b9ad8e3496940544
Jayanth <sadamjay4@gmail.com>
2026-07-03 16:25:44 +0530

    exception filter & interceptors

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,42 +1,46 @@
-import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from './user.service';
+import type { User } from './user.service';
 
 @Controller('user')
 export class UserController {
-    // GET /users
+  constructor(private readonly userService: UserService) {}
+
+  // GET /users
   @Get()
-  getUsers(@Query('name') name: string) {
-    return [
-      { id: 1, name: name }
-    ];
+  getUsers(@Query('name') name: string): User[] {
+    return this.userService.findAllUsers(name);
   }
 
   @Get(':id')
-  getUserById(@Param('id') id: string) {
-    return {
-        id,
-        name: 'jay'
-    }
+  getUserById(@Param('id', ParseIntPipe) id: number): User {
+    return this.userService.findOneUser(id);
   }
 
   @Post()
   createUser(@Body() CreateUserDto: CreateUserDto) {
     return {
-        message: 'User created successfully',
-        data: CreateUserDto
-    }
+      message: 'User created successfully',
+      data: CreateUserDto,
+    };
   }
 
   @Put(':id')
   updateUser(@Param('id') id: string, @Body() UpdateUserDto: UpdateUserDto) {
     return {
-        data: {id, ...UpdateUserDto},
-        message: 'User Updated successfully'
-    }
+      data: { id, ...UpdateUserDto },
+      message: 'User Updated successfully',
+    };
   }
-
-
-  
 }
- 
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { LoggerService } from './user.logger';
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly logger: LoggerService) {}
+
+  private users: User[] = [
+    { id: 1, name: 'jay', email: 'jay@dev.com' },
+    { id: 2, name: 'sadam', email: 'sadam@dev.com' },
+  ];
+
+  findAllUsers(name: string = ''): User[] {
+    this.logger.log('Finding all users');
+    return this.users
+      .filter((user) =>
+        user.name.toLocaleLowerCase().includes(name.toLocaleLowerCase()),
+      )
+      .map(({ id, name, email }) => ({ id, name, email }));
+  }
+
+  findOneUser(id: number): User {
+    const user = this.users.find((user) => user.id === id);
+
+    if (!user) {
+      throw new NotFoundException('User Not Found');
+    }
+
+    return user;
+  }
+}
